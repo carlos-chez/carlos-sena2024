d6ec5daddfa854cd13de99c5e14f5320828c08e1
carlos sanchez <carlosalberto957@hotmail.com>
2024-05-12 18:35:32 -0500

    actualizacion 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
     <title>carlos-sena2024</title>
 </head>
 <body>
+    <h1 class="text-3xl text-red-600 font-bold underline">
+        Hello world!
+    </h1>
     <h1>Suplementacion Deportiva Bienvenidos</h1>
+    <ul>
+        @foreach ($productos as $producto)
+            <li>
+                {{ $producto['nombre'] }}
+                ${{ $producto['precio'] }}
+            </li>    
+        @endforeach
+    </ul>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $productos = [
-        ['nombre' => 'proteina 1', 'precio'=> 290000],
-        ['nombre' => 'proteina 2', 'precio'=> 290000],
-        ['nombre' => 'proteina 3', 'precio'=> 290000],
+        ['nombre' => 'proteina gold standart 5lb 1', 'precio' => 292000],
+        ['nombre' => 'creatina platinum cretine 400g 2', 'precio' => 148000],
+        ['nombre' => 'proteina iso 100 5lb 3', 'precio' => 348000],
+        ['nombre' => 'quemador de grasa lipodrene 90 cap 4', 'precio' => 120000],
+        ['nombre' => 'testabolic xtreme 120 cap 5', 'precio' => 128000],
     ];
-    return view('welcome',['proteina'=> $productos]); 
+    return view('welcome', ['productos' => $productos]); 
 });
 
 
